d780200b28c861e7d8b0eb63da0d925ec64bea78
Dav1337 <87816164+Dav1337@users.noreply.github.com>
2024-09-21 15:47:57 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
+let titleText = "ASSEMBLY";
+let currentTitle = "";
+let index = 0;
+
+function updateTitle() {
+    currentTitle = titleText.slice(0, index);
+    document.title = currentTitle;
+
+    if (index < titleText.length) {
+        index++;
+    } else {
+        index = 1; 
+    }
+}
+
+setInterval(updateTitle, 300);
+
 document.getElementById('enter-button').addEventListener('click', function(event) {
     event.preventDefault();
-
     document.querySelector('.full-page-button').style.display = 'none';
     document.getElementById('assembly-screen').style.display = 'block';
 
